9b277ce77b40729fa1cf246e7497a6d21764eb52
juditsoria <160534450+juditsoria@users.noreply.github.com>
2024-06-20 17:44:32 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Todolist from "./todolist";
 
-
 //create your first component
 const Home = () => {
-	return (
-		<div className="d-flex justify-content-center mt-5">
-			<div className="col-5">
-<Todolist/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="d-flex justify-content-center mt-5">
+      <div className="col-5">
+        <Todolist />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,46 +1,102 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-
-const Todolist = () =>{
+const Todolist = () => {
   const host = "https://playground.4geeks.com/todo";
- const user = "judit_alvarez";
+  const user = "judit_alvarez";
+  const [tareas, setTareas] = useState([]);
+  const [nuevaTarea, setNuevaTarea] = useState("");
 
+  // obtener las tareas de la api
+  async function traerTareas() {
+    const uri = `${host}/users/${user}`;
+    const options = { method: "GET" };
+    const response = await fetch(uri, options);
+    if (!response.ok) {
+      console.log("ha habido un error", response.status, response.statusText);
+      return;
+    }
+    const data = await response.json();
+    setTareas(data.todos);
+  }
 
- const [tareas, setTareas] = useState([]);
+  // borrar las tareas
+  async function borrarTarea(id) {
+    const uri = `${host}/todos/${id}`;
+    const options = { method: "DELETE" };
+    const response = await fetch(uri, options);
+    if (!response.ok) {
+      console.log("ha habido un error", response.status, response.statusText);
+      return;
+    }
+    // Eliminar
+    setTareas(tareas.filter((tarea) => tarea.id !== id));
+  }
 
-//  funcion GET
- async function traerTareas (){
-    const uri = `${host}/users/${user}`
-    const options = {method:"GET"}
-    const response = await fetch (uri, options);
-    if (!response.ok){
-            console.log("ha habido un error",response.status, response.statusText);
-     };
-     const data = await response.json();
-     setTareas(data.todos);
- };
- 
+  // añadir nueva tarea
+  async function agregarTarea() {
+    const uri = `${host}/todos/${user}`;
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ label: nuevaTarea, done: false }),
+    };
+    const response = await fetch(uri, options);
+    if (!response.ok) {
+      console.log("ha habido un error", response.status, response.statusText);
+      return;
+    }
+    const nueva = await response.json();
+    setTareas([...tareas, nueva]);
+    setNuevaTarea(""); // Limpiar el campo de entrada
+  }
 
-
- useEffect(()=>{
+  useEffect(() => {
     traerTareas();
- },[])
+  }, []); //  para que se nada mas cargar la pag se ejecute la funcion
 
+  return (
+    <>
+      <h1 className="container justify-content-center text-center border border-info bg-primary d-flex justify-content-center">
+        ToDoList
+      </h1>
 
-    return (
-       <>
-       <h1>ToDoList</h1>
-       <input
-        type="text"
-        placeholder="escribe una tarea"
-        />
-        <button>añadir tarea</button>
-       <div>
-       {tareas.map((item)=>
+      <div class="container overflow-hidden text-center">
+        <div class="row gx-5">
+          <div class="col">
+            <div class="p-3">
+              <input
+                type="text"
+                placeholder="Escribe una tarea"
+                value={nuevaTarea}
+                onChange={(e) => setNuevaTarea(e.target.value)}
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3">
+              {" "}
+              <button className="btn btn-info" onClick={agregarTarea}>
+                Añadir tarea
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {tareas.map((item) => (
+          <div key={item.id} className="tarea-container">
             <h4>{item.label}</h4>
-        )}
-       </div>
-       </>
-    );
+
+            <button
+              className="delete-button btn btn-danger"
+              onClick={() => borrarTarea(item.id)}
+            >
+              Xgit add .
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
+
 export default Todolist;
